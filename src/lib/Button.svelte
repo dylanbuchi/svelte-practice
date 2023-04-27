26f264f401e0688a1724f0fe31495664d0a82196
Dylan Buchi <devybuchi@gmail.com>
2023-04-27 18:05:02 -0300

    feat(lib): create Button component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let label = "Button";
+</script>
+
+<button>{label}</button>
+
+<style>
+  button {
+    cursor: pointer;
+    padding: 1rem 1.8rem;
+    border-radius: 12px;
+    border: none;
+    background: rgb(31, 70, 149);
+    color: white;
+    font-weight: bold;
+  }
+
+  button:hover {
+    opacity: 0.85;
+  }
+
+  button:active {
+    opacity: 1;
+  }
+</style>
