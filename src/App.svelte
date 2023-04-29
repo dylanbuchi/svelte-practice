75895d551dcc4e680ab519333f101b707f594dfd
Dylan Buchi <devybuchi@gmail.com>
2023-04-29 13:20:47 -0300

    feat(app): add todos

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,18 @@
+<svelte:options immutable />
+
 <script lang="ts">
+  import { v4 as uuid } from "uuid";
   import TodoList from "./lib/todos/TodoList.svelte";
+  import { getTodos } from "./lib/todos/todos.constants";
+
+  let todos = getTodos();
+
+  function addTodo(event: CustomEvent) {
+    const title = event.detail.title;
+    const text = event.detail.text;
+
+    todos = [{ id: uuid(), title, text, done: false }, ...todos];
+  }
 </script>
 
-<TodoList />
+<TodoList {todos} on:addTodo={addTodo} />
